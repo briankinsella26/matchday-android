d171bd21a70ea91c3d64b6d37de01f2cc5a5c8c7
Brian Kinsella <brian.kinsella@distilled.ie>
2022-11-01 18:49:10 +0000

    added match update option added local memory store

--- app/src/main/java/ie/wit/matchday/activities/MatchActivity.kt
+++ app/src/main/java/ie/wit/matchday/activities/MatchActivity.kt
@@ -20,6 +20,8 @@ class MatchActivity : AppCompatActivity() {
     var match = MatchModel()
     private lateinit var refreshIntentLauncher : ActivityResultLauncher<Intent>
     lateinit var app: MainApp
+    var isUpdate: Boolean = false
+    var radioButtonIndex: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,22 +36,46 @@ class MatchActivity : AppCompatActivity() {
 
         i("Match Activity started...")
 
+        if (intent.hasExtra("match_edit")) {
+            if(binding.homeGame.isChecked) {
+                radioButtonIndex = 1;
+            }
+            isUpdate = true
+            match = intent.extras?.getParcelable("match_edit")!!
+            binding.matchOpponent.setText(match.opponent)
+            binding.radioLocation.check(radioButtonIndex)
+            binding.btnAdd.text = getString(R.string.button_saveMatch)
+
+        }
+
 
         binding.btnAdd.setOnClickListener() {
             match.opponent = binding.matchOpponent.text.toString()
             match.result = binding.result.text.toString()
             match.homeOrAway = homeOrAway(binding.awayGame.isChecked)
-            if (match.opponent.isNotEmpty()) {
-                app.matches.add(match.copy())
-                setResult(RESULT_OK)
-                val launcherIntent = Intent(this, MatchListActivity::class.java)
-                refreshIntentLauncher.launch(launcherIntent)
-            }
-            else {
-                Snackbar
-                    .make(it,"Please Enter an Opponent", Snackbar.LENGTH_LONG)
+
+            if (match.opponent.isEmpty()) {
+                Snackbar.make(
+                    it,
+                    getString(R.string.opponent_validation_message),
+                    Snackbar.LENGTH_LONG
+                )
                     .show()
+            } else if (match.homeOrAway.isEmpty()) {
+                Snackbar.make(
+                    it,
+                    getString(R.string.homeOrAway_validation_message),
+                    Snackbar.LENGTH_LONG
+                )
+            } else {
+                if ( isUpdate ) {
+                    app.matches.update(match.copy())
+                } else {
+                    app.matches.create(match.copy())
+                }
             }
+            setResult(RESULT_OK)
+            finish()
         }
     }
 
@@ -61,7 +87,8 @@ class MatchActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.item_delete -> {
-               //todo
+                app.matches.delete(match)
+                finish()
             }
             R.id.item_cancel -> {
                 val launcherIntent = Intent(this, HomepageActivity::class.java)
--- app/src/main/java/ie/wit/matchday/models/MatchModel.kt
+++ app/src/main/java/ie/wit/matchday/models/MatchModel.kt
@@ -1,10 +1,16 @@
 package ie.wit.matchday.models
 
-data class MatchModel(var opponent: String = "",
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class MatchModel(var id: Long = 0,
+                      var opponent: String = "",
                       var homeOrAway: String = "home",
 //                      var lat: Double = 0.0,
 //                      var lng: Double = 0.0,
 //                      var zoom: Float = 0f,
-                      var result: String = "")
+                      var result: String = "") : Parcelable
 
-data class Location(var lat: Double = 0.0, var lng: Double = 0.0, var zoom: Float = 0f)
+@Parcelize
+data class Location(var lat: Double = 0.0, var lng: Double = 0.0, var zoom: Float = 0f) : Parcelable
